ddff6555ba09444adea00ecd72ff7b8562ef7b83
MartinYordanov374 <martinyordanov284@gmail.com>
2025-04-05 17:21:32 +0300

    Update gitignore, add mongo connection config

--- backend/MongooseConfiguration/MongooseConfig.js
+++ backend/MongooseConfiguration/MongooseConfig.js
@@ -2,4 +2,22 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config()
 
-mongoose.connect(process.env.MONGOOSE_CONNECTION_URI)
+
+const connectToMongoDatabase = async () => 
+{
+    try{
+        await mongoose.connect(process.env.MONGOOSE_CONNECTION_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        
+        console.log('Successfully connected to the database')
+    }
+    catch(err)
+    {
+        console.log('There was an error connecting to the db', err)
+    }
+   
+}
+
+module.exports = { connectToMongoDatabase }
