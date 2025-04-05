1ea5825c0827bfca8e6ea286dcfcca270e1eb556
MartinYordanov374 <martinyordanov284@gmail.com>
2025-04-05 17:33:42 +0300

    Add express session and cors configuration

--- backend/ExpressServer/Server.js
+++ backend/ExpressServer/Server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const cors = require('cors')
+const session = require('express-session')
+
 const {loadEnvFile} = require('process')
 loadEnvFile('../.env')
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.FRONTEND_ORIGIN,
+    credentials: true
+}))
+
+app.use(session({
+    cookie:{
+        maxAge: 60*60*24*7,
+        httpOnly: true
+    },
+    saveUninitialized: false,
+    path: '/'
+}))
 app.listen((process.env.NODE_SERVER_PORT), () => {
     console.log('server is listening on port:', process.env.NODE_SERVER_PORT)
 })
